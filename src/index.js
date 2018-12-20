fa6066d317fb58d7a7fb0b4ca2ad01640676be51
KrysieDee <krystal.smithx@gmail.com>
2018-12-20 09:27:41 -0500

    react homeowrk

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import Happy from './components/Happy';
+import Sleepy from './components/Sleepy';
+import Guilty from './components/Guilty';
+import Nav from './components/Nav';
+
+// your code goes here
+class Home extends Component{
+  render(){
+    return(
+<BrowserRouter>
+  <div>
+    <Route exact path="/" component={Nav}>Home</Route>
+    <Route path="/happy" component={Happy}>Happy</Route>
+    <Route path="/guilty" component={Guilty}>Guilty</Route>
+    <Route path="/sleepy" component={Sleepy}>Sleepy</Route>
+  </div>
+</BrowserRouter>    )
+  }
+}
+
+ReactDOM.render( <Home />, document.getElementById('root') );
